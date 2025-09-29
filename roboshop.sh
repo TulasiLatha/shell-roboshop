6866c1202bd183642a7a30425eb5a4f445075b60
TulasiLatha <lathagangadhari98@gmail.com>
2025-09-29 09:10:26 +0530

    session-6

--- roboshop.sh
+++ roboshop.sh
@@ -2,19 +2,41 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-01d0099ea92b83c7f"
+ZONE_ID="Z03045722IKV24M08ZSGG" # replace with your ID
+DOMAIN_NAME="tulasidevops.fun"
 
-for instance in $@
+for instance in $@ # mongodb redis mysql
 do
-     INSATNCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Test}]' --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
-       #get private id
+    # Get Private IP
+    if [ $instance != "frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws86s.fun
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME" # daws86s.fun
+    fi
 
-       if [ $instance != "frontend" ]; then
-             IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-       else
-             IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-        fi
+    echo "$instance: $IP"
 
-        echo "$instance: $IP"
-    done
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }
+    '
+done
 
